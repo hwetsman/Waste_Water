889a950fc1f8b88a04d719be0c4329624a74eafb
hwetsman <howard@tocdr.com>
2022-03-18 18:16:11 -0500

    Refactor: code for backfilling quantities

--- waste_water.py
+++ waste_water.py
@@ -44,16 +44,25 @@ for i in range(idx_first,df.index.max()+1,1):
         new_total = base+delta
         df.loc[i,'start_quantity'] = int(new_total)
 
-print(df)
-
 
 #infer old values
 to_calculate = df[~df.start_quantity.isna()].date_start.tolist()
 for dat in to_calculate:
     calc_idx = df[df.date_end==dat].index[0]
     base_idx = df[df.date_start==dat].index[0]
-    print(calc_idx,base_idx)
-print(to_calculate)
+    change_factor = df.loc[calc_idx,'ptc_15d']
+    base = df.loc[base_idx,'start_quantity']
+    start_amt = base/(1+(change_factor/100))   
+    df.loc[calc_idx,'start_quantity'] = start_amt
+
+# fig = plt.Figure(figsize=(8,4))
+# plt.title('New Orleans Covid Waste Water Testing Data')
+# plt.xlabel('Date')
+# plt.ylabel('Viral Load (not to actual scale)')
+# plt.xticks(rotation=70)
+# X = df.date_start.tolist()
+# Y = df.start_quantity.tolist()
+# plt.plot(X,Y)
 
 
 
