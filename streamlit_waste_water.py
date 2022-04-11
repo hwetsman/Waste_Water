90c8af39f537a888061b4aceb8fb26fbc8511438
hwetsman <howard@tocdr.com>
2022-04-11 08:27:10 -0500

    Refactor: add list of all states

--- streamlit_waste_water.py
+++ streamlit_waste_water.py
@@ -19,7 +19,8 @@ import streamlit as st
 
 
 #select state
-states = ['Louisiana','Florida','Colorado','Idaho']
+# states = ['Louisiana','Florida','Colorado','Idaho']
+states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Washington DC', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 state = st.sidebar.selectbox('State of Interest',states)
 st.write(state)
 url = f'https://data.cdc.gov/resource/2ew6-ywp6.json?wwtp_jurisdiction={state}'
