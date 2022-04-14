caa314fa00910fda28f1f1d441ed7aa201f30d56
hwetsman <howard@tocdr.com>
2022-04-14 15:14:34 -0500

    Refactor: get rid of old code

--- streamlit_waste_water.py
+++ streamlit_waste_water.py
@@ -79,27 +79,21 @@ df = df[df.wwtp_id == plant]
 df = df[['wwtp_id', 'county_names',  'date_start', 'date_end', 'ptc_15d']]
 df = df[~df.ptc_15d.isna()]
 df.reset_index(drop=True,inplace=True)
-# st.write(type(df.loc[0,'date_start']))
 df.sort_values('date_end', inplace=True, axis=0)
 df.drop_duplicates(inplace=True)
 for i,r in df.iterrows():
     df.loc[i,'date_start'] = pd.to_datetime(df.loc[i,'date_start'], format='%Y-%m-%d')
     df.loc[i,'date_end'] = pd.to_datetime(df.loc[i,'date_end'], format='%Y-%m-%d')
-# st.write(df)
 
 #get first and last day
 starting_dates = df.date_start.tolist()
 ending_dates = df.date_end.tolist()
 first_day = str(np.array(starting_dates).min()).split(' ')[0]
 last_day = str(np.array(ending_dates).max()).split(' ')[0]
-# st.write(first_day)
-# st.write(last_day)
 
 # calculate quantity for first end date forward as start
 df.reset_index(inplace=True,drop=True)
 idx_first = df[df.date_start == df.date_end.min()].index[0]
-# st.write(idx_first)
-# st.write(df.index.max())
 for i in range(idx_first, df.index.max()+1, 1):
     if i == idx_first:
         df.loc[i, 'start_quantity'] = 1
@@ -125,52 +119,3 @@ plt.xticks(rotation=70)
 st.pyplot(fig)
 
 
-#     for plant in plant_dict:
-#         df = plant_dict.get(plant)
-#         county = df.county_names.tolist()[0]
-#         df.reset_index(drop=True, inplace=True)
-#         # see if there's enough data
-#         
-#         start_dates = df.date_start.tolist()
-#         first_day = str(np.array(start_dates).min()).split(' ')[0]
-#         
-#         enough = False
-#         for dat in end_dates:
-#             if dat in start_dates:
-#                 enough = True
-    
-#         # calculate quantity for first end date forward as start
-#         idx_first = df[df.date_start == df.date_end.min()].index[0]
-#         for i in range(idx_first, df.index.max()+1, 1):
-#             if i == idx_first:
-#                 df.loc[i, 'start_quantity'] = 10000
-#             else:
-#                 base = df.loc[i-1, 'start_quantity']
-#                 percent_change = df.loc[i, 'ptc_15d']
-#                 delta = df.loc[i, 'ptc_15d']/100*base
-#                 new_total = base+delta
-#                 df.loc[i, 'start_quantity'] = int(new_total)
-    
-#         # infer old values
-#         to_calculate = df[~df.start_quantity.isna()].date_start.tolist()
-#         for dat in to_calculate:
-#             calc_idx = df[df.date_end == dat].index[0]
-#             base_idx = df[df.date_start == dat].index[0]
-#             change_factor = df.loc[calc_idx, 'ptc_15d']
-#             if change_factor == -100:
-#                 change_factor = -99
-#             base = df.loc[base_idx, 'start_quantity']
-#             start_amt = base/(1+(change_factor/100))
-#             df.loc[calc_idx, 'start_quantity'] = start_amt
-    
-        
-#         # create figure
-#         fig = plt.figure(figsize=(12, 5))
-#         X = df.date_start.tolist()
-#         Y = df.start_quantity.tolist()
-#         plt.plot(X, Y,label=plant)
-#         plt.title(f'{county} Covid Waste Water Testing Data from {first_day} to {last_day}')
-#         plt.xlabel('Date')
-#         plt.ylabel('Viral Load (not to actual scale)')
-#         plt.legend()
-#         plt.xticks(rotation=70)
